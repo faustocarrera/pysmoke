7784112446e68801baebe3c7d07abcf4e3df4095
Fausto Carrera <fausto.carrera.f@gmail.com>
2018-07-24 17:22:56 -0300

    remove all files

--- helper/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-Helper entry point
-"""
-
-from .testconfig import TestConfig
-from .appconfig import AppConfig
-from .apicalls import ApiCalls
--- smoke/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-Pysmoke entry point
-"""
-
-from .smoketests import SmokeTests
-from .utils import Utils
