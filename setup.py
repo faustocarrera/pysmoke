8fb3e758c2560f2633bf40296f778775ceec867c
Fausto Carrera <fausto.carrera.f@gmail.com>
2018-07-27 17:58:39 -0300

    add setup file

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from setuptools import find_packages
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = open('README.md').read()
+
+version = '1.0.0'
+requirements = [
+    'requests>=2.4',
+    'click>=4.1',
+    'pyopenssl>=17.5',
+    'ndg-httpsclient>=0.4',
+    'pytest>=3.5'
+]
+
+setup(
+    name='pysmoke',
+    version=version,
+    install_requires=requirements,
+    author='Fausto Carrera',
+    author_email='fausto.carrera@gmx.com',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://faustocarrera.github.io/pysmoke/',
+    license='GNU GENERAL PUBLIC LICENSE',
+    description='A command line tool to run smoke tests in your REST API using python and just config files.',
+    long_description=long_description,
+    entry_points={
+        'console_scripts': [
+            'pysmoke=pysmoke:cli'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Utilities',
+    ]
+)
